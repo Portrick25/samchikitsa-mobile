678ac6711cd6c303afd5995f97f990d87a52d64e
Ritik jha <31991503+Portrick25@users.noreply.github.com>
2020-07-29 19:43:32 +0530

    Update myapp3.js

--- myapp3.js
+++ myapp3.js
@@ -1,3 +1,26 @@
+const offAppformMobile = document.querySelector('#offlineAppointmentMobile');
+
+
+
+
+// database setup for doctor at your doorstep module
+//saving data
+
+offAppformMobile.addEventListener('submit', (e) => {
+   e.preventDefault();
+   db.collection('offlineappointment').add({
+     name: offAppformMobile.name.value,
+     phno: offAppformMobile.phone.value,
+     email: offAppformMobile.email.value,
+     address: offAppformMobile.address.value,
+     location: offAppformMobile.city.value,
+     date: offAppformMobile.date.value,
+     time: offAppformMobile.time.value,
+
+   });
+   alert("data is uploaded successfully. we will contact you soon.")
+})
+
 const offAppform = document.querySelector('#offlineAppointment');
 
 
