4c55bf479ee5f61cc43d8ded8b864e4cd33fc483
Ritik jha <31991503+Portrick25@users.noreply.github.com>
2020-07-29 19:44:50 +0530

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,3 +1,44 @@
+const skypeformMobile = document.querySelector('#skypeuserMobile');
+const zoomformMobile = document.querySelector('#zoomuserMobile');
+const whatsappformMobile = document.querySelector('#whatsappuserMobile');
+
+
+
+
+// database setup for homeopathic teleconsultation
+//saving data
+
+skypeformMobile.addEventListener('submit', (e) => {
+   e.preventDefault();
+   db.collection('skypeid').add({
+     name: skypeformMobile.name.value,
+     userid: skypeformMobile.skypeuserid.value,
+   });
+   alert("data is uploaded successfully. we will contact you soon")
+})
+
+
+zoomformMobile.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('zoom').add({
+    name: zoomformMobile.name.value,
+    userid: zoomformMobile.zoomuserid.value,
+  });
+  alert("data is uploaded successfully. we will contact you soon")
+})
+
+
+whatsappformMobile.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('whatsapp').add({
+    name: whatsappformMobile.name.value,
+    userid: whatsappformMobile.Wno.value,
+  });
+  alert("data is uploaded successfully. we will contact you soon")
+})
+
+
+
 const skypeform = document.querySelector('#skypeuser');
 const zoomform = document.querySelector('#zoomuser');
 const whatsappform = document.querySelector('#whatsappuser');
